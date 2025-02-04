43da58a8804f912758e2edfd6224244e3cb19ff2
Felipe Castro <castro.felipe@ufrgs.br>
2025-02-04 13:18:20 -0300

    Adding a Check

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from src.analysis import analyze_data
 from src.visualization import plot_data
 
 def main():
+    
     filepath = 'data/title.basics.tsv'  # File path
     df = load_data(filepath)
-    df_clean = clean_data(df)
-    genre_counts, decade_counts = analyze_data(df_clean)
-    plot_data(genre_counts, decade_counts)
+
+    if df is not None:  # Check if data was loaded successfully
+        df_clean = clean_data(df)
+        genre_counts, decade_counts = analyze_data(df_clean)
+        plot_data(genre_counts, decade_counts)
+    else:
+        print("Data loading failed. Exiting the program.")
 
 if __name__ == '__main__':
     main()
