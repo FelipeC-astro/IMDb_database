369593f1b25744cd588663a28f6259dc303f2ca3
Felipe Castro <castro.felipe@ufrgs.br>
2025-02-05 13:04:03 -0300

    clean_data error fixed

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def clean_data(df):
-    df = df[df['titleType'] == 'movie']  # Filter only movies
+    df = df[df['titleType'] == 'movie'].copy()  # Filter only movies and make a copy
     df.dropna(subset=['startYear', 'genres'], inplace=True)  # Remove missing values
     df['startYear'] = pd.to_numeric(df['startYear'], errors='coerce')  # Convert year
     return df
